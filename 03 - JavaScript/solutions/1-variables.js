3a9ae70eee80a3f623fdb9243d94198ec857e400
Dan Jansson <dan.jansson@hyxtra.se>
2021-02-17 22:23:14 +0100

    Added JavaScript exercises.

---
+++ 03 - JavaScript/solutions/1-variables.js
@@ -0,0 +1,83 @@
+﻿
+function var1() {
+    console.log('Jag heter Oscar')
+}
+
+function var2() {
+    let number = 42
+    console.log(number)
+}
+
+function var3() {
+    let number = 42
+    console.log("Svaret är " + number)
+}
+
+function var4() {
+    let x = 77
+    let y = 44
+    x = 55
+    let z = x + y
+    console.log('Svar: ' + z)
+}
+
+function var5() {
+
+    const x = 77;
+    let y = 44;
+    x = 55; // TypeError: Assignment to constant variable
+
+    let z = x + y;
+    console.log('Svar: ' + z);
+}
+
+function var6() {
+    let nummer1 = "77";
+    let nummer2 = "50";
+    console.log(nummer1 + nummer2); // "7750"
+}
+
+function var7() {
+    let nummer1 = "77";
+    let nummer2 = 50;
+    nummer1 = parseInt(nummer1);
+    console.log(nummer1 + nummer2);  // 127
+}
+
+function var8() {
+    let nummer = 42;
+    console.log("Mitt favorittal är " + nummer);
+}
+
+function var9() {
+    let nummer = 42;
+    console.log(`Mitt favorittal är ${nummer}`);   
+}
+
+function var10() {
+    let middag = 'broccoli';
+    let Middag = 'pasta';
+    console.log(middag);
+}
+
+function var11() {
+    let x = 12;
+    console.log(typeof x); // number
+
+    let y = true;
+    console.log(typeof y); // boolean
+
+    let z = 'måndag';
+    console.log(typeof z); // string
+
+    let w;
+    console.log(typeof w); // undefinied
+}
+
+function var12() {
+    let x = parseInt('oscar');
+    console.log(x); // "NaN"
+
+    let y = isNaN(x);
+    console.log(y);  // true
+}
---
+++ Exercises/JavaScript/Exercise1.md
@@ -0,0 +1,15 @@
+# Övning 1 - Variabler
+
+## Setup
+
+Om du har en fungerande virtuell server kan du skippa stegen nedan och gå direkt till övningen.
+
+* Använd projektet "03 - JavaScript" som grund/bas för denna övning.
+* Kopiera mappen _"03 - JavaScript"_ till den mapp där du har dina labbar.
+* Öppna den kopierade mappen i VS Code.
+* Kör kommandot `npm install` i terminalfönstret eller i en kommandoprompt (CMD). OBS! Du behöver se till att du är i rätt mapp, dvs i den kopierade mappen när du kör kommandot.
+* Kör kommandot `npm start` för att starta den lokala webbservern. När den startat kommer en ny webbläsare att öppnas med adressen _localhost:10001_ eller liknande.
+
+## Övningar
+
+* Samtliga övningar ligger i mappen [03 - JavaScript](../../03 - JavaScript).
